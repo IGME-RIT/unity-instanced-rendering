f9a12f642365761ea8d07ef284908f201e71beff
Alexander Amling (RIT Student) <aja7073@ad.rit.edu>
2019-08-09 16:23:21 -0400

    Instance Rendering Unity Demo Upload

---
+++ Instance Rendering/Assets/InstanceRendering.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InstanceRendering : MonoBehaviour
+{
+    // instance rendering only allows for 0 to 1023 objects to be rendered in one call, so we'll clamp the editor to those values
+    [Range(0,1023)]
+    public int instanceCount = 1000;
+    
+    public Mesh instanceMesh;
+
+    // any material will work for the way we will be doing instance rendering as long as it has "Enable GPU Instancing" checked
+    // this is not always the case with instance rendering, and we will cover this in the next demo
+    public Material instanceMaterial;
+
+    Matrix4x4[] instanceTransforms;
+    
+    void Start()
+    {
+        instanceTransforms = new Matrix4x4[instanceCount];
+        GenerateTransforms();
+    }
+    
+    void Update()
+    {
+        Graphics.DrawMeshInstanced(instanceMesh, 0, instanceMaterial, instanceTransforms, instanceCount);
+    }
+
+    /// <summary>
+    /// This method exists to generate an array of transform matricies with random positions rotations and scales
+    /// </summary>
+    private void GenerateTransforms()
+    {
+        Vector3 pos = new Vector3();
+        Quaternion rot = new Quaternion();
+        Vector3 scale = new Vector3();
+        for (int i = 0; i < instanceCount; i++)
+        {
+            pos = Random.insideUnitSphere * 50;
+            rot = Random.rotation;
+            scale = new Vector3(Random.Range(0,2.0f), Random.Range(0, 2.0f), Random.Range(0, 2.0f));
+            instanceTransforms[i] = Matrix4x4.TRS(pos, rot, scale);
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,1 +1,17 @@
-# unity-instance-rendering
+Documentation Author: Alexander Amling
+
+# Unity Instance Rendering
+* Unity Compute Shaders Part 1*
+
+The aim of instance rendering is to reduce the number of draw calls by rendering all of the copies of a single mesh at the same time, each with their own respective transform.
+
+At its simplest, instance rendering in Unity is fairly straightforward. The function Graphics.DrawMeshInstanced() Calls for a fairly simple list of parameters: 
+    Any mesh, 
+    The index of the submesh (which for our needs is 0), 
+    A material to use (any material with "Enable GPU Instancing" checked works)
+    An array of 4x4 Matrix transforms
+    A count of the instances we are rendering
+
+Our code calls a simple method GenerateTransforms() to populate the matrix array with random positions rotations and scales, then makes the render each update.
+
+One of the limitations of DrawMeshInstanced is that modifying the transform matricies in their current state is rather innefecient, and this will be addressed in part 3. In Part 2, we will adress the main issue with DrawMeshInstanced; there is a maximum of 1023 instances that can be drawn in one call.
